4d7dc66c16b677ba3891c40e034832fe27e782fc
Bailey Tincher <btincher99@gmail.com>
2020-11-01 16:00:08 -0600

    Fix build of package with tsc

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,6 @@
 import { test, describe, jest, expect } from '@jest/globals';
 
-import ShallotAWS from '../src/aws';
+import { ShallotAWS } from '../src';
 
 describe('ShallotAWS', () => {
   test('Empty Middleware', async () => {
