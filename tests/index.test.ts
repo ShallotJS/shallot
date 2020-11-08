ead901ff85e16719e44931a64a7e9d7732f8ed8d
Bailey Tincher <btincher99@gmail.com>
2020-11-08 13:57:56 -0600

    Add basic tests for middleware

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,10 +1,49 @@
+import type { ShallotMiddlewareHandler, ShallotMiddlewareWithOptions } from '../src/aws';
+import type { Context, Handler } from 'aws-lambda';
+
 import { test, describe, jest, expect } from '@jest/globals';
 
 import { ShallotAWS } from '../src';
 
 describe('ShallotAWS', () => {
-  test('Empty Middleware', async () => {
-    const handler = ShallotAWS(jest.fn()).use(jest.fn());
-    expect(handler instanceof Function);
+  const mockContext: Context = {
+    callbackWaitsForEmptyEventLoop: false,
+    functionName: '',
+    functionVersion: '',
+    invokedFunctionArn: '',
+    memoryLimitInMB: '',
+    awsRequestId: '',
+    logGroupName: '',
+    logStreamName: '',
+    getRemainingTimeInMillis: () => 0,
+    done: () => undefined,
+    fail: () => undefined,
+    succeed: () => undefined,
+  };
+  const mockHandler: Handler<unknown, string> = async () => 'unused';
+
+  test('Handler executes', async () => {
+    const wrappedHandler = ShallotAWS(mockHandler);
+
+    const res = await wrappedHandler(undefined, mockContext, jest.fn());
+
+    expect(res).toBe(await mockHandler(undefined, mockContext, jest.fn()));
+  });
+
+  test('Handler executes with before/after middleware', async () => {
+    const basicMiddlewareHandler: ShallotMiddlewareHandler = jest.fn(
+      async () => undefined
+    );
+    const basicMiddleware: ShallotMiddlewareWithOptions<unknown, string> = () => ({
+      before: basicMiddlewareHandler,
+      after: basicMiddlewareHandler,
+    });
+
+    const wrappedHandler = ShallotAWS(mockHandler).use(basicMiddleware());
+
+    const res = await wrappedHandler(undefined, mockContext, jest.fn());
+
+    expect(res).toBe(await mockHandler(undefined, mockContext, jest.fn()));
+    expect(basicMiddlewareHandler).toHaveBeenCalledTimes(2);
   });
 });
